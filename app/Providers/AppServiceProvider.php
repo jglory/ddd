2f952452d9b62e44bf8a9139e7c8630a9c6310de
yongmoon jo <yongmoon@hanmail.net>
2023-08-07 22:22:38 +0900

    [#3] Register 요청 바인딩 추가

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -55,7 +55,9 @@ class AppServiceProvider extends ServiceProvider implements DeferrableProvider
             ->needs(\App\Models\Repository\Repository::class)
             ->give(\App\Domains\Article\Repositories\Repository::class);
 
-
+        $this->app->bind(\App\Http\Controllers\Api\Auth\Requests\Register::class, function ($app) {
+            return \App\Http\Controllers\Api\Auth\Requests\Register::createFrom($app->request);
+        });
         $this->app->bind(\App\Http\Controllers\Api\Bbs\Requests\DeleteComment::class, function ($app) {
             return \App\Http\Controllers\Api\Bbs\Requests\DeleteComment::createFrom($app->request);
         });
