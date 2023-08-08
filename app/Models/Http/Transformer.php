b58319c5fc57e7b11cb84718ede4dc7a637f9281
yongmoon jo <yongmoon@hanmail.net>
2023-08-08 17:34:48 +0900

    [#9] Models/Http/Transformer 가 Modules 상속받도록 수정

--- app/Models/Http/Transformer.php
+++ app/Models/Http/Transformer.php
@@ -2,16 +2,14 @@
 
 namespace App\Models\Http;
 
+use App\Modules\Transformer\Transformer as Base;
+
 /**
  * 트랜스포머 베이스 클래스
  * Class Transformer
  * @package App\Models\Http
  */
-abstract class Transformer
+abstract class Transformer extends Base
 {
-    /**
-     * @param mixed $data
-     * @return mixed
-     */
-    abstract public function process(mixed $data): mixed;
+
 }
