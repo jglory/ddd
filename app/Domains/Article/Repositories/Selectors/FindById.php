b306ba18482fbcb18d304c250ca5230e4f119de4
yongmoon jo <yongmoon@hanmail.net>
2023-08-12 18:36:03 +0900

    [#11] repository 에서 반환값 타입 array 에서 collection 으로 변경

--- app/Domains/Article/Repositories/Selectors/FindById.php
+++ app/Domains/Article/Repositories/Selectors/FindById.php
@@ -3,25 +3,29 @@
 namespace App\Domains\Article\Repositories\Selectors;
 
 use App\Domains\Article\Eloquents\Article as ArticleEloquent;
-use App\Models\Dto\Entity as Dto;
 use App\Models\Repository\Selector;
 use App\Models\Repository\Specification;
+use Closure;
+use Illuminate\Support\Collection;
 
 class FindById extends Selector
 {
     /**
      * @param Specification $spec
-     * @return Dto[]
+     * @return Collection
      */
-    public function process(Specification $spec): array
+    public function process(Specification $spec): Collection
     {
-        $result = ArticleEloquent::with('comments')
+        return ArticleEloquent::with('comments')
             ->where('id', $spec->id)
-            ->get();
-        if ($result->isEmpty()) {
-            return [];
-        }
-
-        return [$this->mapper->create($result[0])];
+            ->get()
+            ->map(
+                Closure::bind(
+                   function ($item) {
+                        return $this->mapper->create($item);
+                    },
+                    $this
+                )
+            );
     }
 }
--- app/Domains/Customer/Repositories/Selectors/FindByUserIdentity.php
+++ app/Domains/Customer/Repositories/Selectors/FindByUserIdentity.php
@@ -4,22 +4,26 @@ namespace App\Domains\Customer\Repositories\Selectors;
 
 use App\Domains\Customer\Eloquents\Customer as CustomerEloquent;
 use App\Domains\User\Eloquents\User as UserEloquent;
-use App\Models\Dto\Entity as Dto;
 use App\Models\Repository\Selector;
 use App\Models\Repository\Specification;
+use Closure;
+use Illuminate\Support\Collection;
 
 class FindByUserIdentity extends Selector
 {
-    public function process(Specification $spec): ?Dto
+    public function process(Specification $spec): Collection
     {
-        $result = CustomerEloquent::with('user')
+        return CustomerEloquent::with('user')
             ->where('user_id', function ($query) use ($spec) {
                 $query->select('id')->from((new UserEloquent())->getTable())->where('email', $spec->email);
-            })->first();
-        if (is_null($result)) {
-            return null;
-        }
-
-        return $this->mapper->create($result);
+            })
+            ->map(
+                Closure::bind(
+                    function ($item) {
+                        return $this->mapper->create($item);
+                    },
+                    $this
+                )
+            );
     }
 }
