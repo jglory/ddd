14daaebf18569d9231c19b04567aa1e5724182b4
yongmoon jo <yongmoon@hanmail.net>
2023-08-12 18:36:03 +0900

    [#11] repository 에서 반환값 타입 array 에서 collection 으로 변경

--- app/Handlers/Bbs/GetArticleList.php
+++ app/Handlers/Bbs/GetArticleList.php
@@ -3,11 +3,11 @@
 namespace App\Handlers\Bbs;
 
 use App\Commands\Command;
-use App\Domains\Article\Dtos\Article as ArticleDto;
 use App\Domains\Article\Repositories\Specifications\FindAllWithPaging as FindAllWithPagingSpecification;
 use App\Handlers\Handler;
 use App\Handlers\Bbs\Exceptions\ArticleNotFound as ArticleNotFoundException;
 use App\Models\Repository\Repository;
+use Illuminate\Support\Collection;
 
 /**
  * GetArticleList handler
@@ -27,12 +27,12 @@ class GetArticleList extends Handler
     /**
      * @param Command $command
      *
-     * @return ArticleDto[]
+     * @return Collection
      */
-    public function process(Command $command): array
+    public function process(Command $command): Collection
     {
         $result = $this->repository->find(new FindAllWithPagingSpecification($command->page, $command->pageSize));
-        if (empty($result)) {
+        if ($result->isEmpty()) {
             throw new ArticleNotFoundException();
         }
 
